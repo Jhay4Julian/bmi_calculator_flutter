41f737f6012536ca0cbad95dc07778ddf75490ca
Julian Koripamo <jkoripamo@gmail.com>
2022-06-22 16:52:03 +0100

    applied the onPress function

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,8 @@
 import 'package:bmi_calculator/widgets/icon_content.dart';
 import 'package:flutter/material.dart';
 
+import 'widgets/input_card.dart';
+
 const activeCardColor = Color(0xFF1D1E33);
 const inactiveCardColor = Color(0xFF111328);
 const bottomContainerHeight = 80.0;
@@ -31,38 +33,34 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: GestureDetector(
-                    onTap: () {
+                  child: InputCard(
+                    onPress: () {
                       setState(() {
                         userGender = Gender.male;
                       });
                     },
-                    child: InputCards(
-                      color: userGender == Gender.male
-                          ? activeCardColor
-                          : inactiveCardColor,
-                      cardChild: IconContent(
-                        icon: Icons.male,
-                        title: 'MALE',
-                      ),
+                    color: userGender == Gender.male
+                        ? activeCardColor
+                        : inactiveCardColor,
+                    cardChild: IconContent(
+                      icon: Icons.male,
+                      title: 'MALE',
                     ),
                   ),
                 ),
                 Expanded(
-                  child: GestureDetector(
-                    onTap: () {
+                  child: InputCard(
+                    onPress: () {
                       setState(() {
                         userGender = Gender.female;
                       });
                     },
-                    child: InputCards(
-                      color: userGender == Gender.female
-                          ? activeCardColor
-                          : inactiveCardColor,
-                      cardChild: IconContent(
-                        icon: Icons.female,
-                        title: 'FEMALE',
-                      ),
+                    color: userGender == Gender.female
+                        ? activeCardColor
+                        : inactiveCardColor,
+                    cardChild: IconContent(
+                      icon: Icons.female,
+                      title: 'FEMALE',
                     ),
                   ),
                 ),
@@ -70,24 +68,27 @@ class _InputPageState extends State<InputPage> {
             ),
           ),
           Expanded(
-            child: InputCards(
+            child: InputCard(
+              onPress: () {},
               color: activeCardColor,
-              cardChild: const Text('Filler'),
+              cardChild: const Text(''),
             ),
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: InputCards(
+                  child: InputCard(
+                    onPress: () {},
                     color: activeCardColor,
-                    cardChild: const Text('Filler'),
+                    cardChild: const Text(''),
                   ),
                 ),
                 Expanded(
-                  child: InputCards(
+                  child: InputCard(
+                    onPress: () {},
                     color: activeCardColor,
-                    cardChild: const Text('Filler'),
+                    cardChild: const Text(''),
                   ),
                 ),
               ],
@@ -101,23 +102,6 @@ class _InputPageState extends State<InputPage> {
             child: const Center(child: Text('Calculate')),
           )
         ],
-      ),
-    );
-  }
-}
-
-class InputCards extends StatelessWidget {
-  InputCards({required this.color, required this.cardChild});
-  final Color color;
-  final Widget cardChild;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: const EdgeInsets.all(10.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        color: color,
       ),
     );
   }
