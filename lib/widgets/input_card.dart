87a158cd9c6db54b70582b73fc2cba7845696cee
Julian Koripamo <jkoripamo@gmail.com>
2022-06-22 16:48:16 +0100

    added gesture detector and onPress function to the widget

--- lib/widgets/input_card.dart
+++ lib/widgets/input_card.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
 class InputCard extends StatelessWidget {
-  InputCard({required this.color, required this.cardChild});
+  InputCard(
+      {required this.color, required this.cardChild, required this.onPress});
   final Color color;
   final Widget cardChild;
+  final VoidCallback onPress;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: const EdgeInsets.all(10.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        color: color,
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: cardChild,
+        margin: const EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0),
+          color: color,
+        ),
       ),
     );
   }
