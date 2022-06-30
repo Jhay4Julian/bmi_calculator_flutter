f23f9e97adad9614c21e4104e97e75ad412d8505
Julian Koripamo <jkoripamo@gmail.com>
2022-06-30 11:23:55 +0100

    added the funtion to get result

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -5,8 +5,20 @@ class CalculatorBrain {
   final int height;
   final int weight;
 
+  late double _bmi;
+
   String CalculateBMI() {
-    double bmi = weight / pow(height / 100, 2);
-    return bmi.toStringAsFixed(1);
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
   }
 }
